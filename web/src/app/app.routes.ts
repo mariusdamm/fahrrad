a8debc5f1147ec23eb998bee21f748dc376ed015
Marius Damm <mariusdamm1010@gmail.com>
2025-01-24 23:03:23 +0100

    update app.config.ts and app.routes.ts

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardScreenComponent} from "./screens/dashboard-screen/dashboard-screen.component";
+import {LoginScreenComponent} from "./screens/login-screen/login-screen.component";
+import {RegisterScreenComponent} from "./screens/register-screen/register-screen.component";
+import {AuthGuardService} from "./services/auth-guard.service";
 
-export const routes: Routes = [];
+export const publicRoutes: Routes = [
+  {path: 'login', component: LoginScreenComponent, title: 'Login'},
+  {path: 'register', component: RegisterScreenComponent, title: 'Register'},
+]
+
+export const privateRoutes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardScreenComponent, title: 'Dashboard', canActivate: [AuthGuardService]},
+]
+
+export const routes: Routes = [...publicRoutes, ...privateRoutes];

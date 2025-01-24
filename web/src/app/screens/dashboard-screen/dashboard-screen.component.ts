f1a7fd69fc6c56e61aa0cf4211a1714fc723044d
Marius Damm <mariusdamm1010@gmail.com>
2025-01-24 23:05:29 +0100

    add dashboard-screen, login-screen and register-screen

---
+++ web/src/app/screens/dashboard-screen/dashboard-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-screen',
+  imports: [],
+  templateUrl: './dashboard-screen.component.html',
+  styleUrl: './dashboard-screen.component.css'
+})
+export class DashboardScreenComponent {
+
+}
---
+++ web/src/app/screens/register-screen/register-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-screen',
+  imports: [],
+  templateUrl: './register-screen.component.html',
+  styleUrl: './register-screen.component.css'
+})
+export class RegisterScreenComponent {
+
+}

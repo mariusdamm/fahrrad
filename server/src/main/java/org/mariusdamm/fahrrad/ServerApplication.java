c1c61649ee27e2c9d843d5d56fba0d6c173d8f6c
Marius Damm <mariusdamm1010@gmail.com>
2025-01-24 22:56:43 +0100

    add CommandLineRunner

--- server/src/main/java/org/mariusdamm/fahrrad/ServerApplication.java
+++ server/src/main/java/org/mariusdamm/fahrrad/ServerApplication.java
@@ -1,7 +1,13 @@
 package org.mariusdamm.fahrrad;
 
+import org.mariusdamm.fahrrad.dao.RoleRepository;
+import org.mariusdamm.fahrrad.entity.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +16,16 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository) {
+		return args -> {
+			if (!roleRepository.findAll().isEmpty()) {
+				return;
+			}
+
+			roleRepository.save(new Role(0, "ADMIN", new ArrayList<>()));
+			roleRepository.save(new Role(0, "USER", new ArrayList<>()));
+		};
+
+	}
 }

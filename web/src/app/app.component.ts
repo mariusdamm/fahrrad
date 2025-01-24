c2b966603a72886a1e23b1c4058cebd229d402c9
Marius Damm <mariusdamm1010@gmail.com>
2025-01-24 23:18:27 +0100

    m

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {AuthService} from './services/auth.service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FahrradFrontend';
+export class AppComponent implements OnInit, OnDestroy {
+
+  isLoggedIn: boolean | undefined;
+  private loggedInSubscription?: Subscription;
+  currentRoute: string = '';
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  isActive(route: string): boolean {
+    return this.currentRoute === route;
+  }
+
+  ngOnInit() {
+    this.loggedInSubscription = this.authService.isLoggedIn
+      .subscribe(isLoggedIn => {
+        this.isLoggedIn = isLoggedIn;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.loggedInSubscription)
+      this.loggedInSubscription.unsubscribe();
+  }
 }

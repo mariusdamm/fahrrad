f0fd11df451c2be89018b67372ce468ef26f4789
Marius Damm <mariusdamm1010@gmail.com>
2025-01-24 22:12:25 +0100

    add CsrfCookieFilter, KeyGeneratorUtility, RSAKeyProperties, SecurityConfig and SpaCsrfTokenRequestHandler

---
+++ server/src/main/java/org/mariusdamm/fahrrad/util/KeyGeneratorUtility.java
@@ -0,0 +1,25 @@
+package org.mariusdamm.fahrrad.util;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+public class KeyGeneratorUtility {
+
+    private KeyGeneratorUtility() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static KeyPair generateRsaKey() {
+        KeyPair keypair;
+
+        try {
+            KeyPairGenerator keypairGenerator = KeyPairGenerator.getInstance("RSA");
+            keypairGenerator.initialize(4096);
+            keypair = keypairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new IllegalStateException("Error generating RSA key pair.", e);
+        }
+
+        return keypair;
+    }
+}

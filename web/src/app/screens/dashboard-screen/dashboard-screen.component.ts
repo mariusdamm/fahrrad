f340e8d3ce53232d40f97d69435e8c6f7e5421b2
Marius Damm <mariusdamm1010@gmail.com>
2025-01-25 14:14:01 +0100

    add fetchDrives()

--- web/src/app/screens/dashboard-screen/dashboard-screen.component.ts
+++ web/src/app/screens/dashboard-screen/dashboard-screen.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DriveDto} from '../../dtos/drive-dto';
+import {AxiosService} from '../../services/axios.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-screen',
@@ -7,6 +9,33 @@ import {DriveDto} from '../../dtos/drive-dto';
   templateUrl: './dashboard-screen.component.html',
   styleUrl: './dashboard-screen.component.css'
 })
-export class DashboardScreenComponent {
+export class DashboardScreenComponent implements OnInit {
   drives: DriveDto[] = [];
+
+  constructor(
+    private readonly axiosService: AxiosService,
+    private readonly authService: AuthService,
+  ) {
+  }
+
+  ngOnInit(){
+    this.fetchDrives();
+  }
+
+  fetchDrives() {
+    this.axiosService.request(
+      "GET",
+      "/drives/",
+      ""
+    ).then(response => {
+      return response.data;
+    }).then(drives => {
+      if (drives == null)
+        throw Error("drives is null")
+      this.drives = drives;
+    }).catch(error => {
+      if (error.response.status === 401)
+        this.authService.deleteJwtToken();
+    });
+  }
 }

cd0ff350ec68897a020e3411d9a782df492e3c49
Marius Damm <mariusdamm1010@gmail.com>
2025-01-25 11:47:51 +0100

    update dashboard

--- web/src/app/screens/dashboard-screen/dashboard-screen.component.ts
+++ web/src/app/screens/dashboard-screen/dashboard-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DriveDto} from '../../dtos/drive-dto';
 
 @Component({
   selector: 'app-dashboard-screen',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-screen.component.css'
 })
 export class DashboardScreenComponent {
-
+  drives: DriveDto[] = [];
 }

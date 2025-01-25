2982d70ae49fa0ab4bf03a3128a8b373f6e15165
Marius Damm <mariusdamm1010@gmail.com>
2025-01-25 15:23:58 +0100

    add createDrive in server

--- server/src/main/java/org/mariusdamm/fahrrad/controller/DriveController.java
+++ server/src/main/java/org/mariusdamm/fahrrad/controller/DriveController.java
@@ -1,5 +1,6 @@
 package org.mariusdamm.fahrrad.controller;
 
+import org.mariusdamm.fahrrad.dto.DriveDto;
 import org.mariusdamm.fahrrad.entity.AppUser;
 import org.mariusdamm.fahrrad.entity.Drive;
 import org.mariusdamm.fahrrad.service.AppUserService;
@@ -7,16 +8,14 @@ import org.mariusdamm.fahrrad.service.DriveService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/drives")
 public class DriveController {
-    
+
     private final DriveService driveService;
     private final AppUserService appUserService;
 
@@ -24,16 +23,28 @@ public class DriveController {
         this.driveService = driveService;
         this.appUserService = appUserService;
     }
-    
-    @GetMapping
+
+    @GetMapping("")
     public ResponseEntity<Collection<Drive>> getYearlyDrivesOfUser(){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         AppUser user = appUserService.getUserByUsername(username);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        
+
         Collection<Drive> drives = driveService.getYearlyDrivesOfUser(user);
         return ResponseEntity.status(HttpStatus.OK).body(drives);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Drive> createDrive(@RequestBody DriveDto drive) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        AppUser user = appUserService.getUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        Drive new_drive = driveService.createDrive(drive, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new_drive);
     }
 }

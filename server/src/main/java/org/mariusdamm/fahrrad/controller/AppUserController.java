12cee79fde2c1d529516fbb0801ae93df56a16ad
Marius Damm <mariusdamm1010@gmail.com>
2025-01-25 13:45:46 +0100

    add AppUserController

---
+++ server/src/main/java/org/mariusdamm/fahrrad/controller/AppUserController.java
@@ -0,0 +1,30 @@
+package org.mariusdamm.fahrrad.controller;
+
+import org.mariusdamm.fahrrad.dto.AppUserDto;
+import org.mariusdamm.fahrrad.entity.AppUser;
+import org.mariusdamm.fahrrad.service.AppUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AppUserController {
+
+    private final AppUserService appUserService;
+
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @GetMapping("/self")
+    public ResponseEntity<AppUserDto> getSelf() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        AppUser user = appUserService.getUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(user.toDto());
+    }
+}
